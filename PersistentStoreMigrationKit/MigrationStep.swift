bac338dce01006703d416eb5e24520c23a2436c6
Georg C. Brückmann <gcb@bloo7.com>
2015-10-07 14:33:34 +0200

    Add documentation comments to MigrationStep

--- PersistentStoreMigrationKit/MigrationStep.swift
+++ PersistentStoreMigrationKit/MigrationStep.swift
@@ -9,13 +9,24 @@
 import Foundation
 import CoreData
 
+/// A `MigrationStep` instance encapsulates the migration from one `NSManagedObjectModel` to another without any intermediate models.
+/// Migration is performed via an `NSMigrationManager` using an `NSMappingModel`.
 final class MigrationStep: NSObject {
+	/// Specifies how to from `sourceModel` to `destinationModel`.
 	let mappingModel: NSMappingModel
+	/// The model to migrate from.
 	let sourceModel: NSManagedObjectModel
+	/// The model to migrate to.
 	let destinationModel: NSManagedObjectModel
 	
 	private var keyValueObservingContext = NSUUID().UUIDString
 	
+	/// Initializes a migration step for a source model, destination model, and a mapping model.
+	/// 
+	/// - Parameters:
+	///   - sourceModel: The model to migrate from.
+	///   - destinationModel: The model to migrate to.
+	///   - mappingModel: Specifies how to from `sourceModel` to `destinationModel`.
 	init(sourceModel: NSManagedObjectModel, destinationModel: NSManagedObjectModel, mappingModel: NSMappingModel) {
 		self.sourceModel = sourceModel
 		self.destinationModel = destinationModel
@@ -24,6 +35,15 @@ final class MigrationStep: NSObject {
 	
 	private var progress: NSProgress?
 	
+	/// Performs the migration from the persistent store identified by `sourceURL` and using `sourceModel` to `destinationModel`, saving the result in the persistent store identified by `destinationURL`.
+	/// 
+	/// Inserts an `NSProgress` instance into the current progress tree.
+	/// 
+	/// - Parameters:
+	///   - sourceURL: Identifies the persistent store to migrate from.
+	///   - sourceStoreType: A string constant (such as `NSSQLiteStoreType`) that specifies the source store type.
+	///   - destinationURL: Identifies the persistent store to migrate to. May be identical to `sourceURL`.
+	///   - destinationStoreType: A string constant (such as `NSSQLiteStoreType`) that specifies the destination store type.
 	func executeForStoreAtURL(sourceURL: NSURL, type sourceStoreType: String, destinationURL: NSURL, storeType destinationStoreType: String) throws {
 		progress = NSProgress(totalUnitCount: 100)
 		defer { progress = nil }

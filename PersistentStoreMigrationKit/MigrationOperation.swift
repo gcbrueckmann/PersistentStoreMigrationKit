d44f0872653689cef2b69d22e60ef66d99dfaae8
Georg C. Brückmann <gcb@bloo7.com>
2015-10-07 14:54:28 +0200

    Add documentation comments to MigrationOperation

--- PersistentStoreMigrationKit/MigrationOperation.swift
+++ PersistentStoreMigrationKit/MigrationOperation.swift
@@ -9,28 +9,47 @@
 import Foundation
 import CoreData
 
+/// A `MigrationOperation` instance encapsulates the progressive migration from one `NSManagedObjectModel` to another with an arbitrary number of intermediate models.
+/// This is a companion to and implemented on top of the `MigrationPlan` class.
 public final class MigrationOperation: NSOperation {
+	/// Identifies the persistent store to migrate from.
 	public var sourceURL: NSURL!
+	/// A string constant (such as `NSSQLiteStoreType`) that specifies the source store type.
 	public var sourceStoreType: String!
+	/// Identifies the persistent store to migrate to. May be identical to `sourceURL`.
 	public var destinationURL: NSURL!
+	/// A string constant (such as `NSSQLiteStoreType`) that specifies the destination store type.
 	public var destinationStoreType: String!
+	/// The model to migrate to.
 	public var destinationModel: NSManagedObjectModel!
+	/// A list of bundles to search for the source model and intermediate models.
 	public var bundles: [NSBundle]!
+	/// The overall progress of the migration operation.
 	public let progress: NSProgress
-	public private(set) var error: NSError?
+	/// Any error that may have occured during the execution of the migration operation.
+	public private(set) var error: ErrorType?
 	
+	/// Initializes a migration operation.
+	/// 
+	/// Inserts an `NSProgress` instance into the current progress tree.
 	override public required init() {
 		progress = NSProgress(totalUnitCount: 100)
 		super.init()
 	}
 	
+	/// Defines possible migration operation states.
 	@objc public enum State: Int {
+		/// The migration operation is ready to execute.
 		case Ready
+		/// The migration operation is executing.
 		case Executing
 		case Failed
+		/// The migration operation has finished executing.
 		case Finished
+		/// The migration operation has been cancelled.
 		case Cancelled
 	}
+	/// The current state of the migration operation.
 	private(set) public dynamic var state = State.Ready
 	
 	private func cancelWithError(error: NSError) {
